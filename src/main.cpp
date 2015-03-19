1d4ed2d3a525fd5b7167f2cef20d26640829fe0c
jmacey <jmacey@bournemouth.ac.uk>
2015-03-19 08:37:18 +0000

    Updated for latest NGL build system

--- src/main.cpp
+++ src/main.cpp
@@ -226,7 +226,7 @@ SDL_GLContext createOpenGLContext(SDL_Window *window)
   // under centos
   #ifdef DARWIN
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
-    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 2);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 1);
     SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
     SDL_GL_SetAttribute(SDL_GL_ACCELERATED_VISUAL, 1);
   #else
